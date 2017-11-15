45841cd80cc9a10a8a6d1e83a856eeb000151b9b
Reece Kidd <reecekidd95@gmail.com>
2017-11-15 06:49:36 +0000

    Basic express server working

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,17 @@
-console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined')) //Allows Morgan to print out logs in certain way. 
+app.use(bodyParser.json()) //Allow our app to parse JSON easily. 
+app.use(cors()) //This allows any client to access this. This is a security risk.
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello world!'
+    })
+})
+
+app.listen(process.env.PORT || 8081)
