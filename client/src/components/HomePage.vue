72a64b84c75d6acb4d5ce3ac21facee71f2d6ae0
Reece Kidd <reecekidd95@gmail.com>
2017-11-18 19:14:06 +0000

    Restarted work on user authentication

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -2,8 +2,8 @@
       <div class="container-fluid">
        <div class="row center">
          <div class="col-xs-12">
-           <button type="button" class="btn btn-primary" @click="navigateTo({ name : 'login'})"> Login </button>
-           <button type="button" class="btn btn-primary" @click="navigateTo({ name : 'register'})"> Register </button>
+           <button type="button" class="btn btn-primary btn-lg" @click="navigateTo({ name : 'login'})"> Login </button>
+           <button type="button" class="btn btn-primary btn-lg" @click="navigateTo({ name : 'register'})"> Register </button>
            </div>
        </div>
       </div>
--- server/src/app.js
+++ server/src/app.js
@@ -1,87 +1,77 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const exphbs = require('express-handlebars');
-const expressValidator = require('express-validator');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const mongo = require('mongodb');
-const mongoose = require('mongoose');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginapp');
-var db = mongoose.Connection;
+mongoose.createConnection('mongodb://localhost/loginapp');
+var db = mongoose.connection;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-//Initialize the app.
-const app = express()
+// Init App
+var app = express();
 
-//View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout'}));
-app.set('view engine', 'handlebars');
-
-//Bodyparse middleware - set up code and confidgurations.
-app.use(morgan('combined')) //Allows Morgan to print out logs in certain way. 
-app.use(bodyParser.json()) //Allow our app to parse JSON easily. 
+// BodyParser Middleware
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors()) //This allows any client to access this. This is a security risk.
 
-//Public folder where style sheets and images will be stored.
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Middleware for expression session
+// Express Session
 app.use(session({
     secret: 'secret',
     saveUninitialized: true,
     resave: true
 }));
 
-// Passport initilization
+// Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Express Validator
 app.use(expressValidator({
-    errorFormatter: function(param, msg, value) {
-        var namespace = param.split('.')
-        , root    = namespace.shift()
-        , formParam = root;
-  
-      while(namespace.length) {
-        formParam += '[' + namespace.shift() + ']';
-      }
-      return {
-        param : formParam,
-        msg   : msg,
-        value : value
-      };
-    }
-  }));
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
-  // Connect Flash
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
 app.use(flash());
 
-// Global Variables for flash messages
+// Global Vars
 app.use(function (req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    res.locals.user = req.user || null;
-    next();
-  });
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
-//Middleware for route files
+
+
 app.use('/', routes);
 app.use('/users', users);
 
+// Set Port
 app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function(){
