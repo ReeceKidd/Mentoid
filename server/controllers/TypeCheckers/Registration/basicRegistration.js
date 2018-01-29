ff16d3410393ca2ef5c4fa486afede9639237e11
Reece Kidd <reecekidd95@gmail.com>
2018-01-29 09:13:53 +0000

    Got type checker working for basic registration

--- server/controllers/TypeCheckers/Registration/basicRegistration.js
+++ server/controllers/TypeCheckers/Registration/basicRegistration.js
@@ -1,10 +1,9 @@
-module.exports = function basicRegistration(req){
-        var values = Object.values(req)
-        console.log(values)
-        for(var index in values){
-            if(!values[index] instanceof String){
-                return values[index] + ' must be a string'
-            }
+module.exports = function basicRegistration(req) {
+    var errMessages = []
+    for (var property in req) {
+        var value = req[property];
+        if (typeof value !== 'string') {
+            return 'The property (' + property + ') containing value (' + value.toString() + ') must be a string' 
         }
-    
+    }
 }
